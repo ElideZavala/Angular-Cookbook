ef9b87b8ca4ae59f8fbb085c123ff2f86a7eb9d4
Elide <zavalavinagreelide@gmail.com>
2023-07-01 17:16:06 -0600

    feat: :hammer: Etiquetas @optional y @SkipSelf
    Se agregan directivas para el manejo de un servicion y que afecte solo aun componente o modulo

--- chapter03/start_here/ng-singleton-service/src/app/app.module.ts
+++ chapter03/start_here/ng-singleton-service/src/app/app.module.ts
@@ -4,20 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotificationsButtonComponent } from './components/notifications-button/notifications-button.component';
-import { NotificationsService } from './services/notifications.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NotificationsButtonComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-  ],
-  providers: [
-    NotificationsService
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, NotificationsButtonComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
