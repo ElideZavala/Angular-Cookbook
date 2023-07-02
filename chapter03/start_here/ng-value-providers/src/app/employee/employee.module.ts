ef965f3f17c4844a440c54ffeaca2d49c5ee56c9
Elide <zavalavinagreelide@gmail.com>
2023-07-02 14:57:15 -0600

    feat: :zap: Valores de injection en la aplicacion
    Se ingresa un injection del valor de canActive, este sera false en el componente de empleados, por lo que no les permitira eliminar los elementos

--- chapter03/start_here/ng-value-providers/src/app/employee/employee.module.ts
+++ chapter03/start_here/ng-value-providers/src/app/employee/employee.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { EmployeeComponent } from './employee.component';
-
+import { EmployeeConfig } from './constants/employee-config';
+import { APP_CONFIG } from '../constants/app-config';
 
 @NgModule({
   declarations: [EmployeeComponent],
-  imports: [
-    CommonModule,
-    EmployeeRoutingModule,
-    SharedModule
-  ]
+  imports: [CommonModule, EmployeeRoutingModule, SharedModule],
+  providers: [
+    {
+      provide: APP_CONFIG,
+      useValue: EmployeeConfig,
+    },
+  ],
 })
-export class EmployeeModule { }
+export class EmployeeModule {}
