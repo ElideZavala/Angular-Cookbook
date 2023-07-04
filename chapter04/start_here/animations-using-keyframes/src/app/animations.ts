d4d8c195278bc6308dc225fedb4d3784a84dac88
Elide <zavalavinagreelide@gmail.com>
2023-07-04 13:33:35 -0600

    feat: :zap: Sea crea animacion en angular usando Keyframe propio de la libreria

--- chapter04/start_here/animations-using-keyframes/src/app/animations.ts
+++ chapter04/start_here/animations-using-keyframes/src/app/animations.ts
@@ -4,21 +4,38 @@ import {
   style,
   animate,
   transition,
+  keyframes,
 } from '@angular/animations';
 
 export const cardAnimation = trigger('cardAnimation', [
-  state('active', style({
-    color: 'rgb(51, 51, 51)',
-    backgroundColor: 'white'
-  })),
-  transition('void => *', [
+  state(
+    'active',
     style({
-      transform: 'translateX(-200px)',
-      opacity: 0
-    }),
-    animate('0.2s ease', style({
-      transform: 'translateX(0)',
-      opacity: 1
-    }))
+      color: 'rgb(51, 51, 51)',
+      backgroundColor: 'white',
+    })
+  ),
+  transition('void => *', [
+    animate(
+      '1.5s 0s ease',
+      keyframes([
+        style({
+          transform: 'translateX(-200px) scale3d(0.4, 0.4, 0.4)',
+          offset: 0,
+        }),
+        style({
+          transform: 'translateX(0px) rotate(-90deg) scale3d(0.5, 0.5, 0.5)',
+          offset: 0.25,
+        }),
+        style({
+          transform: 'translateX(-200px) rotate(90deg) scale3d(0.6, 0.6, 0.6)',
+          offset: 0.5,
+        }),
+        style({
+          transform: 'translateX(0) ',
+          offset: 1,
+        }),
+      ])
+    ),
   ]),
-])
+]);
