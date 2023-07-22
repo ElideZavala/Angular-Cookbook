7dd7ab81a7e43b3bff231a4c7e8c7366d8102c03
Elide <zavalavinagreelide@gmail.com>
2023-07-22 11:48:52 -0600

    feat(rxjs): :sparkles: se crea un metodo startStream para el learning de map y merge de rxjs
    Utilizamos la libreria de rxjs para hacer pruebas del operador map cada que un valor es emitido con interval y juntar todo con un merge

--- chapter05/start_here/rxjs-operators-instance-methods/src/app/app.component.ts
+++ chapter05/start_here/rxjs-operators-instance-methods/src/app/app.component.ts
@@ -1,30 +1,43 @@
 import { Component } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
+import { map, merge } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   subscription: Subscription = null;
   inputStreamData = ['john wick', 'inception', 'interstellar'];
+  cartoonsStreamData = ['thunder cats', 'Dragon Ball Z', 'Ninja Turtles'];
   outputStreamData = [];
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   startStream() {
     const streamSource = interval(1500);
-    this.subscription = streamSource.subscribe(input => {
-      this.outputStreamData.push(input);
-    });
+    const cartonStreamSource = interval(1000).pipe(
+      map((output) => output % this.cartoonsStreamData.length),
+      map((index) => this.cartoonsStreamData[index])
+    );
+
+    this.subscription = streamSource
+      .pipe(
+        // el remanente de la division de output / input.length
+        map((output) => output % this.inputStreamData.length),
+        // output 0,1,2,0,1,2,0,1,2,0,1,2,0,1,2
+        map((index) => this.inputStreamData[index]),
+        // output john wick, inception, interstellar, john ...
+        merge(cartonStreamSource)
+      )
+      .subscribe((element) => {
+        this.outputStreamData.push(element);
+      });
   }
 
   stopStream() {
     this.subscription.unsubscribe();
     this.subscription = null;
   }
-
-
 }
