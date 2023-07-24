76430f7b810df4007411d9a6d3ec507fb95b95e2
Elide <zavalavinagreelide@gmail.com>
2023-07-23 19:54:45 -0600

    feat(rxjs): :zap: Se crea metodo para actualizar valores del Form inmediatamente
    Implementamos rxjs para realizar mejoras en el Form, haciendo que automaticamente se actualizen los valores del form, eliminando igual el botton de submit

--- chapter05/start_here/using-combinelatest-operator/src/app/home/home.component.ts
+++ chapter05/start_here/using-combinelatest-operator/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { combineLatest, Observable } from 'rxjs';
+import { map, startWith, tap } from 'rxjs/operators';
 import { COLORS } from '../constants';
 
 @Component({
@@ -10,22 +12,22 @@ import { COLORS } from '../constants';
 export class HomeComponent implements OnInit, OnDestroy {
   sizeOptions = [100, 200, 300, 400, 500, 600, 700];
   colorOptions = COLORS;
-  borderRadiusOptions = [4, 6, 8, 10, 12, 14, 16, 18, 20]
+  borderRadiusOptions = [4, 6, 8, 10, 12, 14, 16, 18, 20];
 
-  boxStyles: {
-    width: string,
-    height: string,
-    backgroundColor: string,
-    color: string
-    borderRadius: string
-  }
+  boxStyles$: Observable<{
+    width: string;
+    height: string;
+    backgroundColor: string;
+    color: string;
+    borderRadius: string;
+  }>;
 
   boxForm = new FormGroup({
     size: new FormControl(''),
-    borderRadius: new FormControl(''),
-    textColor: new FormControl(''),
     backgroundColor: new FormControl(''),
-  })
+    textColor: new FormControl(''),
+    borderRadius: new FormControl(''),
+  });
 
   constructor() {}
 
@@ -34,27 +36,36 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.boxForm.get('backgroundColor').setValue(this.colorOptions[0]);
     this.boxForm.get('textColor').setValue(this.colorOptions[1]);
     this.boxForm.get('borderRadius').setValue(this.borderRadiusOptions[0]);
-    this.applyChanges();
+    this.listenToInputChanges();
   }
-
-  setBoxStyles(size, backgroundColor, color, borderRadius) {
-    this.boxStyles = {
-      width: `${size}px`,
-      height: `${size}px`,
-      backgroundColor,
-      color,
-      borderRadius: `${borderRadius}px`
-    }
-  }
-
-  applyChanges() {
-    this.setBoxStyles(
-      this.boxForm.get('size').value,
-      this.boxForm.get('backgroundColor').value,
-      this.boxForm.get('textColor').value,
-      this.boxForm.get('borderRadius').value,
-    )
+  listenToInputChanges() {
+    this.boxStyles$ = combineLatest([
+      this.boxForm
+        .get('size')
+        .valueChanges.pipe(startWith(this.sizeOptions[0])),
+      this.boxForm
+        .get('backgroundColor')
+        .valueChanges.pipe(startWith(this.colorOptions[0])),
+      this.boxForm
+        .get('textColor')
+        .valueChanges.pipe(startWith(this.colorOptions[1])),
+      this.boxForm
+        .get('borderRadius')
+        .valueChanges.pipe(startWith(this.borderRadiusOptions[0])),
+    ]).pipe(
+      map(([size, backgroundColor, textColor, borderRadius]) => {
+        return {
+          width: `${size}px`,
+          height: `${size}px`,
+          backgroundColor,
+          color: textColor,
+          borderRadius: `${borderRadius}px`,
+        };
+      })
+    );
   }
 
   ngOnDestroy() {}
 }
+
+/* Se usa startWith y una vez que carge el obsevable se desaparce */
